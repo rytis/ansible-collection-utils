a15ebcd2184e00b75b0e960cea6512634cba9f04
Rytis Sileika <rts.pub@pulegium.eu>
2021-03-15 21:33:08 +0000

    almost straight pass through to lexicon

--- plugins/modules/dns_client.py
+++ plugins/modules/dns_client.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
 from ansible.module_utils.basic import AnsibleModule
+from lexicon.client import Client
+from lexicon.config import ConfigResolver
 
 
-class DnsClient():
-    def __init__(self):
-        pass
+SUPPORTED_RECORDS = ["A", "AAAA", "CNAME", "MX", "NS", "SOA", "TXT", "SRV"]
 
 
 def main():
     module = AnsibleModule(
-        argument_spec={}
+        argument_spec={
+            "provider_name": {"type": "str", "required": True},
+            "action": {"type": "str", "required": True},
+            "domain": {"type": "str", "required": True},
+            "type": {"type": "str", "choices": SUPPORTED_RECORDS},
+            "name": {"type": "str"},
+            "content": {"type": "str"},
+            "delegated": {"type": "str", "default": None},
+            "provider_options": {"type": "dict", "required": True},
+        }
     )
-    dns_client = DnsClient()
+    action = {
+        "provider_name": module.params["provider_name"],
+        "action": module.params["action"],
+        "delegated": module.params["delegated"],
+        "domain": module.params["domain"],
+        "type": module.params["type"],
+        "name": module.params["name"],
+        "content": module.params["content"],
+        module.params["provider_name"]: module.params["provider_options"],
+    }
+    config = ConfigResolver().with_dict(action)
+    Client(config).execute()
     module.exit_json()
 
 
